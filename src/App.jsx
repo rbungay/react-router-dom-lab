f2eb904616c0bac9bcfe867b237503955404fe3c
Rani Bungay <ranibungay@gmail.com>
2024-10-19 15:03:14 -0400

    create initial state and function to add pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
 import "./App.css";
 import { NavBar } from "./components/Navbar.jsx";
 import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
+
+const initialState = [
+  { _id: 1, boxholder: "Alex", boxSize: "Small" },
+  { _id: 2, boxholder: "Bob", boxSize: "Medium" },
+  { _id: 3, boxholder: "Marley", boxSize: "Small" },
+  { _id: 4, boxholder: "Superman", boxSize: "Large" },
+  { _id: 5, boxholder: "Widow", boxSize: "Medium" },
+];
 
 const App = () => {
+  const [mailboxes, setMailboxes] = useState(initialState);
+
+  const addBox = (newMailboxData) => {
+    newMailboxData._id = mailboxes.length + 1;
+    setMailboxes([...mailboxes, newMailboxData]);
+  };
+
   return (
     <>
-      <h1>My mailboxes</h1>
       <NavBar />
       <Routes>
         <Route
           path="/"
           element={
-            <h2>This takes us to home page component. Will change after </h2>
+            <main>
+              <h1>Post Office</h1>
+            </main>
           }
         ></Route>
         <Route
@@ -25,6 +42,10 @@ const App = () => {
           element={
             <h2>This will be for the newMailbox. import will come later </h2>
           }
+        ></Route>
+        <Route
+          path="/mailboxes/:mailboxId"
+          element={<h2>This will be for the specific mailbox later. </h2>}
         ></Route>
       </Routes>
     </>
