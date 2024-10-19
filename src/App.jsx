b50b025fa3b8163350d5d385038300d2d2d4060d
Rani Bungay <ranibungay@gmail.com>
2024-10-19 14:33:11 -0400

    create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import "./App.css";
-import { NavBar } from "./components/NavBar";
+import { NavBar } from "./components/Navbar.jsx";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <h1>Hello world!</h1>
+      <h1>My mailboxes</h1>
       <NavBar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <h2>This takes us to home page component. Will change after </h2>
+          }
+        ></Route>
+        <Route
+          path="/mailboxes"
+          element={
+            <h2>This will be for the mailboxlist. import will come later </h2>
+          }
+        ></Route>
+        <Route
+          path="/new-mailbox"
+          element={
+            <h2>This will be for the newMailbox. import will come later </h2>
+          }
+        ></Route>
+      </Routes>
     </>
   );
 };
